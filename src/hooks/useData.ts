3136d93353be6405a5b5af1c83b176529b5f61f6
Enrico Zappalà <zpp.enrico@gmail.com>
2024-01-18 20:12:00 +0100

    Filter games by genre & unique keys fix

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,29 +1,37 @@
 import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
+import { AxiosRequestConfig } from 'axios';
 
 interface FetchResponse<T> {
 	count: number;
 	results: T[];
 }
 
-function useData<T>(endpoint: string) {
+function useData<T>(
+	endpoint: string,
+	requestConfig?: AxiosRequestConfig,
+	deps?: any[]
+) {
 	const [data, setData] = useState<T[]>([]);
 	const [error, setError] = useState('');
 	const [isLoading, setLoading] = useState(false);
 
-	useEffect(() => {
-		setLoading(true);
-		apiClient
-			.get<FetchResponse<T>>(endpoint)
-			.then((res) => {
-				setData(res.data.results);
-				setLoading(false);
-			})
-			.catch((error) => {
-				setError(error.message);
-				setLoading(false);
-			});
-	}, []);
+	useEffect(
+		() => {
+			setLoading(true);
+			apiClient
+				.get<FetchResponse<T>>(endpoint, { ...requestConfig })
+				.then((res) => {
+					setData(res.data.results);
+					setLoading(false);
+				})
+				.catch((error) => {
+					setError(error.message);
+					setLoading(false);
+				});
+		},
+		deps ? [...deps] : []
+	);
 
 	return { data, error, isLoading };
 }
