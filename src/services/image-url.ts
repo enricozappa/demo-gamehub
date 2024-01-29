a4d106e2f7a531ebccbb992394579a3f6cca65b1
Enrico Zappalà <zpp.enrico@gmail.com>
2024-01-29 18:10:00 +0100

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
 // Adds a crop parameter to image url
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 function getCroppedImageUrl(url: string) {
-	if (!url) return '';
+	if (!url) return noImage;
 
 	const target = 'media/';
 	const index = url.indexOf(target) + target.length;
